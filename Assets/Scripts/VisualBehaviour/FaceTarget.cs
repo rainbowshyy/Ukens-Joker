e649b09207b763d2dbbb63b73133d70829aa1dc2
rainbowshyyDev <99175473+rainbowshyyDev@users.noreply.github.com>
2024-01-31 11:48:59 +0100

    feat: add noise to doors

--- Assets/Scripts/VisualBehaviour/FaceTarget.cs
+++ Assets/Scripts/VisualBehaviour/FaceTarget.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using UkensJoker.DataArchitecture;
 
 namespace UkensJoker.VisualBehaviour
 {
     public class FaceTarget : MonoBehaviour
     {
-        [SerializeField] private Transform target;
+        [SerializeField] private Vector3Reference _target;
+        [SerializeField] private Vector3 _offset;
 
         private void Update()
         {
-            transform.forward = target.position - transform.position;
+            transform.forward = _target.Value + _offset - transform.position;
         }
     }
 }
